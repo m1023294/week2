d844d12a6f005c48addf0d7124adc42db25baec0
Niranjan Shrishail Hampannavar <M1023294@mindtree.com>
2018-11-26 18:11:55 +0530

    2nd commit

---
+++ src/TestApp.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestApp {
+	public List<String> getNames(){
+		List<String> list=new ArrayList<String>();
+		list.add("Niranjan");
+		list.add("Bhanu");
+		list.add("Vishwa");
+		System.out.println(list);
+		return list;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
